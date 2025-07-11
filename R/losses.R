228f5f11366c06d37022bb7484c256703b78d732
ielbadisy <elbadisyimad@gmail.com>
2025-07-11 10:27:40 +0100

    test custom loss function

--- R/losses.R
+++ R/losses.R
@@ -153,21 +153,14 @@ plot(mod_aft$loss_history, type = "l", main = "aft_loss", ylab = "Loss", xlab = 
 
 
 ## custom
-
 combo_loss <- function(pred, true) {
   time <- true[, 1]
   torch_mean((pred - log(time + 1))^2) + 0.01 * torch_mean(pred)
 }
-
-
-
 
 mod_combo <- survdnn(Surv(time, status) ~ x1 + x2,
                    data = toy_data,
                    .loss_fn = combo_loss,
                    epochs = 100,     
                    verbose = TRUE)
-s
 plot(mod_combo$loss_history, type = "l", main = "combo_loss", ylab = "Loss", xlab = "Epoch")
-
-
