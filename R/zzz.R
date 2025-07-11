01d35a524ebdd797f767fee4433718670264361e
ielbadisy <elbadisyimad@gmail.com>
2025-07-11 15:42:47 +0100

    add .onLoard() to register torch custom ops and set default options

---
+++ R/zzz.R
@@ -0,0 +1,14 @@
+.onLoad <- function(libname, pkgname) {
+  # package level option
+  
+  op <- options()
+  
+  op.survdnn <- list(survdnn.default_epochs = 100)
+  
+  toset <- !(names(op.survdnn) %in% names(op))
+  
+  if (any(toset)) options(op.survdnn[toset])
+}
+
+
+
