dcf531a5d2f96c4d9a166ec1375dab5ba96fdc59
ielbadisy <elbadisyimad@gmail.com>
2025-07-12 17:32:29 +0100

    create a globals.R to centralize internal dependencies

---
+++ R/globals.R
@@ -0,0 +1,7 @@
+#' @import torch
+#' @importFrom survival Surv survfit
+#' @importFrom ggplot2 ggplot aes geom_line labs theme_minimal
+#' @importFrom stats model.frame model.matrix model.response delete.response terms qnorm sd
+#' @importFrom utils tail
+#' @importFrom methods is
+NULL
