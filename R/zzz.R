16d6d670b1fcf15a760c7a528947ae2fd1c3933a
ielbadisy <elbadisyimad@gmail.com>
2025-07-12 18:08:33 +0100

    declare global variables to avoid R CMD check notes

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,14 @@
 .onLoad <- function(libname, pkgname) {
   
-  # package level option
+  # package level default options
   op <- options()
   op.survdnn <- list(survdnn.default_epochs = 100)
   toset <- !(names(op.survdnn) %in% names(op))
   if (any(toset)) options(op.survdnn[toset])
-  
+
+  # declare global variables to avoid R CMD check notes
+  utils::globalVariables(c(
+    "fold", "metric", "value", "id", "time", "surv", "group", "mean_surv",
+    "n", "se", "hidden", "lr", "activation", "epochs", "loss_name", ".loss_fn"
+  ))
 }
-
-
-
