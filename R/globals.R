dd3e3ee704dea5678ca11caf8ce50aaed07f5cdc
ielbadisy <elbadisyimad@gmail.com>
2025-07-15 16:31:26 +0100

    add approx, pnorm, and predict from stats

--- R/globals.R
+++ R/globals.R
@@ -1,7 +1,8 @@
 #' @import torch
 #' @importFrom survival Surv survfit
 #' @importFrom ggplot2 ggplot aes geom_line labs theme_minimal
-#' @importFrom stats model.frame model.matrix model.response delete.response terms qnorm sd
+#' @importFrom stats model.frame model.matrix model.response delete.response terms qnorm sd approx pnorm predict
 #' @importFrom utils tail
 #' @importFrom methods is
+#' @importFrom dplyr all_of
 NULL
